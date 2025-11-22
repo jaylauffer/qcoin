63e2a2b931161d606cc57ab1a7b12413d7d6ecf4
Jay Lauffer <jaylauffer@users.noreply.github.com>
2025-11-22 15:37:54 +0800

    Add helper to build create asset transaction

--- qcoin-types/src/lib.rs
+++ qcoin-types/src/lib.rs
@@ -55,7 +55,15 @@ pub struct TransactionInput {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum TransactionKind {
+    Transfer,
+    CreateAsset,
+    // later: MintAsset, BurnAsset, etc.
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Transaction {
+    pub kind: TransactionKind,
     pub inputs: Vec<TransactionInput>,
     pub outputs: Vec<Output>,
     pub witness: Vec<Vec<u8>>, // raw script/witness data for now
@@ -63,8 +71,43 @@ pub struct Transaction {
 
 impl Transaction {
     pub fn tx_id(&self) -> Hash256 {
-        let serialized = bincode::serialize(self).expect("transaction serialization should be infallible");
+        let serialized =
+            bincode::serialize(self).expect("transaction serialization should be infallible");
         let hash = blake3::hash(&serialized);
         *hash.as_bytes()
     }
+}
+
+pub fn create_asset_transaction(
+    issuer_script_hash: Hash256,
+    kind: AssetKind,
+    metadata_root: Hash256,
+) -> (AssetDefinition, Transaction) {
+    let kind_byte = match kind {
+        AssetKind::Fungible => 0u8,
+        AssetKind::NonFungible => 1u8,
+        AssetKind::SemiFungible => 2u8,
+    };
+
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(&issuer_script_hash);
+    hasher.update(&[kind_byte]);
+    hasher.update(&metadata_root);
+    let asset_id = AssetId(*hasher.finalize().as_bytes());
+
+    let definition = AssetDefinition {
+        asset_id: asset_id.clone(),
+        kind,
+        issuer_script_hash,
+        metadata_root,
+    };
+
+    let transaction = Transaction {
+        kind: TransactionKind::CreateAsset,
+        inputs: vec![],
+        outputs: vec![],
+        witness: vec![],
+    };
+
+    (definition, transaction)
 }
